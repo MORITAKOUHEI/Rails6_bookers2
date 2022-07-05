be84190ed48d60aa646c2a2581b415a1ea4323f4
MORITAKOUHEI <106665501+MORITAKOUHEI@users.noreply.github.com>
2022-07-05 11:37:07 +0000

    [休憩]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ApplicationRecord
   has_many :books
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   # 一覧画面で使う
-  has_many :followings, through: :follower, source: :followed
-  has_many :followers, through: :followed, source: :follower
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -23,15 +23,27 @@ class User < ApplicationRecord
   end
 
   # フォローしたときの処理
-  def follow(user_id)
-    follower.create(followed_id: user_id)
+  def follow(user)
+    relationships.create(followed_id: user.id)
   end
   # フォローを外すときの処理
-  def unfollow(user_id)
-    follower.find_by(followed_id: user_id).destroy
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
   end
   # フォローしているか判定
   def following?(user)
     followings.include?(user)
+  end
+  
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
   end
 end
