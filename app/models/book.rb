379b6a152c083a51157de059464ee8a35cf2a824
MORITAKOUHEI <106665501+MORITAKOUHEI@users.noreply.github.com>
2022-07-06 04:22:59 +0000

    [Finish]応用課題5

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,23 +2,25 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-  
-  def self.search_for(content, method)
-    if method == 'perfect'
-      Book.where(title: content)
-    elsif method == 'forward'
-      Book.where('title LIKE ?', content+'%')
-    elsif method == 'backward'
-      Book.where('title LIKE ?', '%'+content)
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @book = Book.where("title LIKE?","#{word}")
+    elsif search == "forward_match"
+      @book = Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @book = Book.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @book = Book.where("title LIKE?","%#{word}%")
     else
-      Book.where('title LIKE ?', '%'+content+'%')
+      @book = Book.all
     end
   end
 end
